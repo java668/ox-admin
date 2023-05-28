a0473bb49b1129eb8335b06f7a7a29f58d765d6b
陈江龙 <java668_real@163.com>
2023-05-28 17:29:06 +0800

    权限控制

--- ox-admin-application/src/main/java/com/java668/oxadmin/config/secrity/components/CustomAccessDeniedHandler.java
+++ ox-admin-application/src/main/java/com/java668/oxadmin/config/secrity/components/CustomAccessDeniedHandler.java
@@ -22,7 +22,7 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
-        //当用户在没有授权的情况下访问受保护的REST资源时，将调用此方法发送403 Forbidden响应
-        response.sendError(HttpServletResponse.SC_FORBIDDEN, "暂无权限操作，请联系管理员");
+        // 当用户在没有授权的情况下访问受保护的REST资源时，将调用此方法发送403 Forbidden响应
+        response.sendError(HttpServletResponse.SC_FORBIDDEN);
     }
 }
